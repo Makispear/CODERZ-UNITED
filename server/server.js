4e3143703293368647eab2a46e823f4734ccf1f0
Maki Aboabida <maki-miko@hotmail.com>
2022-01-07 16:17:13 -0500

    Connected to mongodb server with mongoose issue (#3)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
+// import express.js -----------------------
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// import the database ---------------------
+const db = require('./config/connection')
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`API server running on port ${PORT}!`));
+});
