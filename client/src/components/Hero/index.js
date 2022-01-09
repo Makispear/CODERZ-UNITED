16a29d17123824bbe007f1f51e9dc814cc33ec79
Maki Aboabida <maki-miko@hotmail.com>
2022-01-09 03:42:46 -0500

    More Responsiveness features to buttons

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -4,14 +4,14 @@ import logo from "../../assets/images/logo.png"
 
 export default function Hero() {
   return (
-    <section className="h-screen">
-      <div className="sm:py-10 mx-3 my-10 p-4 md:p-14 flex flex-wrap w-full justify-center sm:p-16">
-        <div className="sm:w-3/5">
+    <section>
+      <div className="h-max sm:py-10 px-3 my-10 lg:py-10 flex flex-wrap w-full justify-center lg:justify-center sm:p-1 sm:justify-center sm:mx-auto align-top">
+        <div className="w-4/5 sm:w-5/6 md:w-3/5 mt-14">
           <div>
-            <h2 className="text-6xl font-bold sm:m-10 tracking-wider">LEARN WEB DEVELOPMENT FOR FREE</h2>
+            <h2 className="text-5xl sm:text-6xl font-bold mx-auto sm:m-10 tracking-wider">LEARN WEB DEVELOPMENT FOR FREE</h2>
           </div>
           <div>
-            <p className="sm:mx-10 text-2xl text-darkGray">Lorem ipsum here for a paragrapgh that doesn look so bad and something to describe the website</p>
+            <p className="sm:mx-10 text-l mt-5 sm:text-2xl text-darkGray">Lorem ipsum here for a paragrapgh that doesn look so bad and something to describe the website</p>
           </div>
           <div className="sm:mx-10 sm:py-6 flex mt-14">
             <div className=" mr-9">
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,9 +9,9 @@ export default function Nav(props) {
     <header>
       <nav className="w-full flex flex-wrap justify-between p-4 sm:p-6 md:p-12">
         {/* Logo  */}
-        <div className="flex md:px-2 md:px-1 items-center">
+        <div className="flex md:px-1 items-center">
           <img src={logo} alt={`${BrandName} logo`} className="bg-white w-10 mr-2" />
-          <h1 className="w-full text-center text-black font-bold text-xl whitespace-nowrap text-black tracking-wider">{BrandName}</h1>
+          <h1 className="w-full text-center font-bold text-xl whitespace-nowrap text-black tracking-wider">{BrandName}</h1>
         </div>
         {/* Nav links  */}
         <div>
