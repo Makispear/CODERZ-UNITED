25e49c363d6de29c914a8ba2ce96550e552f5cc2
Maki Aboabida <maki-miko@hotmail.com>
2022-01-18 07:12:11 -0500

    heroku again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/project-1', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/CODERZUNTED', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // uncomment this for production 
