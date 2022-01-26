4ebb22e3524b1fa5002d29fbdcd981b630bc3519
Maki Aboabida <maki-miko@hotmail.com>
2022-01-26 09:26:39 -0500

    changed Manifest file

--- client/src/components/Quotes/index.js
+++ client/src/components/Quotes/index.js
@@ -8,8 +8,8 @@ export default function Quotes() {
   }
   const source2 = {
     title: "Apple CEO Tim Cook even said in 2019 that about half of Apple's US employment last year was made up of people who did not have a four-year degree.",
-    source: "Allana Akhtar Updated Dec 27, 2020, 9:39 AM - INSIDER",
-    date: "Updated Dec 27, 2020, 9:39 AM"
+    source: "Lisa Eadicicco - INSIDER",
+    date: "Mar 7, 2019, 11:40 AM"
   }
   const source3 = {
     title: "Google, Apple and 12 other companies that no longer require employees to have a college degree",
@@ -19,31 +19,34 @@ export default function Quotes() {
 
 
   return (
-    <section className="py-10 sm:px-3 my-5
+    <section className="py-10 sm:px-3
                         flex flex-col w-full bg-darkGray">
       <div className="w-full p-10 sm:p-28 flex flex-wrap flex-col justify-between">
+
         <div className="flex justify-around flex-wrap h-full w-full mt-5">
           <div className="grid place-items-center ">
-            <h2 className="font-bold uppercase text-white mt-5">Education is evolving <span className="text-tertiary">..</span></h2>
+            <h2 className="font-bold uppercase text-white mt-5">Listen to the leaders<span className="text-tertiary"> ...</span></h2>
           </div>
           <SingleQuote props={source1} />
         </div>
+
         <div className="flex justify-around flex-wrap h-full w-full mt-5">
           <SingleQuote props={source2} />
           <div className="grid place-items-center ">
-            <h2 className="font-bold text-white uppercase mt-5">There are other ways <span className="text-tertiary">!</span></h2>
+            <h2 className="font-bold text-white uppercase mt-5">And Tech companies<span className="text-tertiary"> !</span></h2>
           </div>
           <div className="flex justify-around flex-wrap h-full w-full mt-5">
             <div className="grid place-items-center ">
-              <h2 className="font-bold text-white uppercase mt-5">Here's another one <span className="text-tertiary">...</span></h2>
+              <h2 className="font-bold text-white uppercase mt-5">And Google<span className="text-tertiary"> ..?</span></h2>
             </div>
             <SingleQuote props={source3} />
           </div>
         </div>
+
       </div>
       <div className="w-full text-white flex flex-wrap justify-center p-4 text-center font-bold">
         <h2 className="uppercase mt-5">
-          Lets revolutionize education <span className="text-tertiary">!</span>
+          What are you waiting for?<a href="/signup" className="link"> Signup </a>/<a href="/login" className="link">Login </a>now.
         </h2>
       </div>
     </section>
