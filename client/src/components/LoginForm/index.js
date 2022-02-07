4841d475f802023e454086ccd42cc84bd372debe
Maki Aboabida <maki-miko@hotmail.com>
2022-02-07 17:00:44 -0500

    Add size to signup an login input fields for readability

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -31,36 +31,36 @@ export default function LoginForm() {
   return (
     <form className=" flex flex-col p-3" onSubmit={handleLoginSubmit}>
       {error &&
-        <div className="flex flex-col p-2 bg-red-500 text-secondary">
+        <div className="flex flex-col p-3 bg-red-500 text-secondary">
           <p>
             {error.message}
           </p>
         </div>
       }
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         {error ?
           <label htmlFor="email" className=" text-xl text-secondary"><span className="text-red-500">* </span>Email</label>
           :
           <label htmlFor="email" className=" text-xl text-secondary">Email</label>
         }
-        <input type="email" name="email" placeholder="someone@example.com" onChange={handleChange} className="p-1" />
+        <input type="email" name="email" placeholder="someone@example.com" onChange={handleChange} className="p-2 primary-radius" />
       </div>
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         {error ?
           <label htmlFor="password" className=" text-xl text-secondary"><span className="text-red-500">* </span>Password</label>
           :
           <label htmlFor="password" className=" text-xl text-secondary">Password</label>
         }
-        <input type="password" name="password" placeholder="●●●●●●●●" className="p-1" onChange={handleChange} />
+        <input type="password" name="password" placeholder="●●●●●●●●" className="p-2 primary-radius" onChange={handleChange} />
       </div>
       {/* <div className="p-2 font-semibold">
         <input type="checkbox" name="trainer-confirm" id="trainer-yes" className="mr-1" />
         <label for="trainer-yes">Remember me</label>
       </div> */}
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         <button className="bg-tertiary font-bold text-white button-style border-2 border-secondary hover:border-tertiary capitalize tracking-wider">Login</button>
       </div>
-      <div className="flex flex-col p-2 mt-6 text-darkGray ">
+      <div className="flex flex-col p-3 mt-6 text-darkGray ">
         <p>New to Coderz United? <a href="/signup" className="link">Sign up.</a></p>
       </div>
     </form>
--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -48,43 +48,43 @@ export default function SignupForm() {
           <p>If you can't login please try again later!</p>
         </div>
       }
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         {error ?
           <label htmlFor="firstName" className="text-xl text-secondary"><span className="text-red-500">* </span>First Name</label>
           :
           <label htmlFor="firstName" className="text-xl text-secondary">First Name</label>
         }
-        <input type="text" name="firstName" id="firstName" placeholder="John" className="p-1 primary-radius" onBlur={handleChange} />
+        <input type="text" name="firstName" id="firstName" placeholder="John" className="p-2 primary-radius" onBlur={handleChange} />
       </div>
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         {error ?
           <label htmlFor="lastName" className="text-xl text-secondary"><span className="text-red-500">* </span>Last Name</label>
           :
           <label htmlFor="lastName" className="text-xl text-secondary">Last Name</label>
         }
-        <input type="text" name="lastName" placeholder="Smith" className="p-1 primary-radius" onBlur={handleChange} />
+        <input type="text" name="lastName" placeholder="Smith" className="p-2 primary-radius" onBlur={handleChange} />
       </div>
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         {error ?
           <label htmlFor="email" className="text-xl text-secondary"><span className="text-red-500">* </span>Email</label>
           :
           <label htmlFor="email" className="text-xl text-secondary">Email</label>
         }
-        <input type="email" name="email" placeholder="someone@example.com" onBlur={handleChange} className="p-1 primary-radius" />
+        <input type="email" name="email" placeholder="someone@example.com" onBlur={handleChange} className="p-2 primary-radius" />
       </div>
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         {error ?
           <label htmlFor="password" className="text-xl text-secondary"><span className="text-red-500">* </span>Password</label>
           :
           <label htmlFor="password" className="text-xl text-secondary">Password</label>
         }
-        <input type="password" name="password" placeholder="●●●●●●●●" className="p-1 primary-radius" onBlur={handleChange} />
+        <input type="password" name="password" placeholder="●●●●●●●●" className="p-2 primary-radius" onBlur={handleChange} />
       </div>
       {/* <div className="p-2 font-semibold">
         <input type="checkbox" name="trainer-confirm" id="trainer-yes" className="mr-1" />
         <label for="trainer-yes">Remember me</label>
       </div> */}
-      <div className="flex flex-col p-2">
+      <div className="flex flex-col p-3">
         <button className="bg-tertiary font-bold text-white button-style border-2 border-secondary hover:border-tertiary capitalize tracking-wider">Sign up </button>
       </div>
       <div className="flex flex-col p-2 mt-6 text-darkGray">
