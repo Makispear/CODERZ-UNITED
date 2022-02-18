d632bf0a861e775f7e9a77f5662d6ee5d19a5343
Maki Aboabida <maki-miko@hotmail.com>
2022-02-18 15:33:05 -0500

    added photo of git bash

--- client/src/pages/AllModules/course_modules/CourseModules.js
+++ client/src/pages/AllModules/course_modules/CourseModules.js
@@ -1,8 +1,4 @@
 import CourseMaterial from "../../../components/CourseMaterial";
-import WelcomeUser from "../../../components/WelcomeUser";
-
-import { goBack } from "../../../utils/previousPage";
-
 
 export default function CourseModules() {
 
--- client/src/pages/AllModules/getting_started_modules/installations/Git.js
+++ client/src/pages/AllModules/getting_started_modules/installations/Git.js
@@ -5,6 +5,8 @@ import { useMutation, useQuery } from "@apollo/client"
 import { GET_COMPLETED_LESSONS } from "../../../../utils/queries"
 import { MARK_COMPLETED_LESSON } from "../../../../utils/mutations"
 import Reference from "../../../../utils/Reference"
+import gitBashInterface from "../../../../assets/images/gitbash_interface.png"
+import { useState } from "react"
 
 export default function Git() {
   const Lesson_title = "Install Git"
@@ -58,6 +60,12 @@ export default function Git() {
 
   }
 
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const toggleImage = (img) => {
+    setIsModalOpen(!isModalOpen)
+  }
+
   return (
     <section className="style-module-section highlight">
 
@@ -71,10 +79,28 @@ export default function Git() {
       </div>
 
       <div className="p-3 sm:p-10 sm:w-600 md:w-700 lg:w-900 flex flex-col gap-5">
-        <p>To start working with Git we need a terminal. If you are using MacOS you should have a terminal already. With windows we'll need to download Git Bash. Git as it's stated in the Git website <q className="font-bold">is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.</q></p>
+        <p>
+          To start working with Git we need a terminal. If you are using MacOS you should have a terminal already. With windows we'll need to download Git Bash. Git as it's stated in the Git website <q className="italic">is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.</q>
+        </p>
 
-        <p>Git helps us control our codebase versions. This can come in handy if we ever break our code. Then we can roll back to the older version on the code and start over. Sometimes when the <span className="">code broke</span>, it's easier to find where it happened when you can look at different versions of that code. We'll look more into Git and how to use it after <Reference props={{ word: "this", ref: "The Getting Started Module" }} /> module.</p>
+        <p>
+          Git helps us control our codebase versions. This can come in handy if we ever break our code. Then we can roll back to the older version on the code and start over. Sometimes when the <span className="">code broke</span>, it's easier to find where it happened when you can look at different versions of that code. We'll look more into Git and how to use it after <Reference props={{ word: "this", ref: "The Getting Started Module" }} /> module.
+        </p>
 
+        <p>
+          Here is the link to <a target="_blank" rel="noopener noreferrer" href="https://git-scm.com/downloads" className="link">download GitBash</a>.</p>
+        <p>
+          After installation is complete launch git bash. It should look something like this:
+        </p>
+      </div>
+
+      <div className={`flex justify-center primary-radius cursor-pointer`}>
+        <img
+          src={gitBashInterface}
+          alt="VS Code Interface from Microsoft's website"
+          className={`cursor-pointer p-2 border border-black ${isModalOpen ? "full-screen-img" : "w-300"}`}
+          onClick={() => toggleImage()}
+        />
       </div>
 
       <div className="flex my-1 justify-between w-full items p-3 sm:p-10 sm:w-600 md:w-700 lg:w-900">
